26a45f57880b4b8730a4a9c7086105ecdda97693
Manthan Kuvadiya <152585227+manthan2876@users.noreply.github.com>
2024-11-20 14:26:16 +0530

    external exam

---
+++ script.js
@@ -0,0 +1,19 @@
+// Close the dropdown if the user clicks outside of it
+function abc() {
+    const x = document.getElementById("a");
+    const y = document.getElementById("b");
+    const z = document.getElementById("c");
+    const p = document.getElementById("d");
+    const q = document.getElementById("e");
+    const r = document.getElementById("f");
+    const v = document.getElementById("l");
+    const g = document.getElementById("d");
+    x.style.opacity = 1;
+    y.style.opacity = 1;
+    z.style.opacity = 1;
+    p.style.opacity = 1;
+    q.style.opacity = 1;
+    r.style.opacity = 1;
+    v.style.opacity = 1;
+    g.style.opacity = 1;
+}
